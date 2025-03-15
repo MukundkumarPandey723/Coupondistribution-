f2cf3c529eeccfcfea5263b1764893f69eba2745
MukundkumarPandey723 <135650045+MukundkumarPandey723@users.noreply.github.com>
2025-03-15 15:48:54 +0530

    Create CouponRepository.java
    Repository Inteface

---
+++ CouponRepository.java
@@ -0,0 +1,9 @@
+package com.Coupon_distribution.CouponDistribution_Assignment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    // ...
+}
